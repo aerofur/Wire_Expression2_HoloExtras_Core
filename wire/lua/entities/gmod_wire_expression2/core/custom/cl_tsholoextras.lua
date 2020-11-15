7912b6fa07a82e69091d62a36a389eb42ea920ce
Titus Studios Media Group <titusstudiosmediagroup@gmail.com>
2020-11-15 15:37:45 +1300

    Update cl_tsholoextras.lua

--- wire/lua/entities/gmod_wire_expression2/core/custom/cl_tsholoextras.lua
+++ wire/lua/entities/gmod_wire_expression2/core/custom/cl_tsholoextras.lua
@@ -1,2 +1,3 @@
 E2Helper.Descriptions["holoDeleteAll(e:)"] = "Removes all holos parented to an entity"
+E2Helper.Descriptions["holoDeleteAllRecursive(e:)"] = "Removes all holos parented to an entity regardless of the owner"
 E2Helper.Descriptions["holoDelete(e:n)"] = "Removes holo at index parented to an entity"
